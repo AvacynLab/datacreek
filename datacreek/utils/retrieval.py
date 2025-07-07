272157fd860d6a9efb95c52c766ef2a3c23629b0
AvacynLab <165329721+AvacynLab@users.noreply.github.com>
2025-07-07 20:29:26 +0200

    Run isort on retrieval

--- datacreek/utils/retrieval.py
+++ datacreek/utils/retrieval.py
@@ -2,8 +2,8 @@
 
 from __future__ import annotations
 
-from typing import Dict, List, Optional
 from collections import Counter
+from typing import Dict, List, Optional
 
 try:
     import hnswlib  # type: ignore
